e17b3bad2e7e2cdb3166d5472afb40ec19fb0463
Volodymyr KOZHEMIAKIN <vkozhemi@e1r1p3.unit.ua>
2019-04-03 20:08:01 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Bomberman
+
+Abstract: Bomberman is one of the most famous video games ever. With more that 70 adaptations, from the first version on MSX, ZX Spectrum and Sharp MZ-700 in 1983 to the last versions available on the PlayStation Network, WiiWare and the Xbox Live Arcade commercialized in 2010. More than 10 millions units have been sold.
+
+The goal of this projet is to create a full implementation of a 3D Bomberman video game.
+
+3D version of Bomberman, functional, and complete with graphics, animations and levels, solo game:
+
+	• The player can acces a main menu allowing him to start a new game, load a save, adjust game settings and exit the game.
+	• Different difficulty 
+	• To beat the game, the player must win 3 levels. 
+	• If the player looses, the game state it clearly and restarting the current level, or issuing a game over. 
+	• Sounds! Musics! The main menu and the 3 levels have their own music. Also, every relevant events have trigger a sound, like a bomb exploding, picking up a power up, killing an ennemi, dying, etc.
+	• The visual assets in 3D, but the gameplay stay 2D like the original game. For instance, if the player dies, the camera shift its angle and spin around him while the death animation plays, and at the beginning of a level, the camera fly around the level before focusing above the player. A good exemple of a 3D game with a 2D gameplay.
+	• Animated 3D models of a bomberman and its ennemies.
+	• As stated in the previous section,  main menu allow to customize the game settings. It is possible to customize at least:
+		◦ The screen resolution
+		◦ Windowed or full screen mode 
+		◦ Key bindings
+		◦ Music and sounds volumes
+	• The game offers a way to save the player’s progression and restore it on demand. 
+
+## Requierements, Install
+
+### MacOsX:
+    - brew update && brew install wget sdl2 sdl2_image sdl2_mixer sdl2_ttf
+
+## Single
+<img src="https://github.com/vkozhemi/Bomberman/raw/master/img/2.png" width="700">
+
+## Multiplayer
+<img src="https://github.com/vkozhemi/Bomberman/raw/master/img/1.png" width="700">
+
+## Game
+<img src="https://github.com/vkozhemi/Bomberman/raw/master/img/3.gif">
+
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <main.hpp>
+#include <list>
+#include <Menu.class.hpp>
+#include <ia.class.hpp>
+#include <wall.class.hpp>
+#include <joystick.hpp>
+#include <save.class.hpp>
+#include <map_event.class.hpp>
+
+Event *	main_event = Factory::create_event(); // GLOBAL
+
+int		main( int ac, char **av ) {
+	int            _time = 0;
+	Ia *           ia_play = NULL;
+	static float   time = 0;
+
+	std::srand(std::time(0));
+
+	try {
+        main_event->save_config = Factory::create_save();
+        ia_play = Factory::create_ia();
+        main_event->menu = Factory::create_menu(main_event);
+        main_event->joystick = Factory::create_joystick();
+        main_event->soundrender = Factory::create_sound_render();
+
+        main_event->init(ac, av); // tout est la pour les autres initializations
+
+        while (true == main_event->event_running) {
+            if ((1 / (clock() - time)) * CLOCKS_PER_SEC > 60) {
+                // usleep(1);
+                continue ;
+            }
+            // usleep(10000);
+            _time++;
+            _time = _time % 60;
+            if (main_event->game_ended >= 0) {
+
+                main_event->game_ended--;
+                main_event->render->render_all(main_event->map, main_event->char_list, NULL);
+                if (main_event->game_ended < 0) {
+                    main_event->game_pause = true;
+                    main_event->mode_menu = true; // desactive menu render
+                    globject::reinit_level(0);
+                }
+                continue;
+            }
+            main_event->joystick->read_key(1);
+						if (main_event->event_running == false)
+							break;
+            ia_play->start(time);
+            if (main_event->event_running == false)
+                break;
+            main_event->dec_timer();
+            Wall::delete_wall_destoyed();
+            time = clock();
+            if (false == main_event->mode_menu) {
+                Map_event::check_warm_up();
+                main_event->render->render_all(main_event->map, main_event->char_list, NULL);
+            }
+            else
+                main_event->menu->main_loop();
+        }
+        main_event->w_log("Delete Main Event then EXIT");
+        if (NULL != main_event) {
+            delete main_event;
+            main_event = NULL;
+        }
+    }
+    catch (std::exception & e){
+        std::cerr << "EXIT_FAILURE " << e.what() << std::endl;
+        return (EXIT_FAILURE);
+    }
+    std::cout << "EXIT_SUCCESS" << std::endl;
+    return (EXIT_SUCCESS);
+}
